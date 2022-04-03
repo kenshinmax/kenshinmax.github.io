f6c1cbc98636b82188364ad1c232f6c37dbe9aa0
Cliff Sanchez <kenshinmax@gmail.com>
2022-04-02 23:02:23 -0400

    updated git

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/cliffsanchez/projects/repos/kenshinmax.github.io/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/cliffsanchez/projects/repos/kenshinmax.github.io/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/cliffsanchez/projects/repos/kenshinmax.github.io/src/pages/index.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("/Users/cliffsanchez/projects/repos/kenshinmax.github.io/src/pages/using-typescript.tsx")),
+  "component---src-templates-blog-post-js": preferDefault(require("/Users/cliffsanchez/projects/repos/kenshinmax.github.io/src/templates/blog-post.js"))
+}
+
